69146ee1eab9ccd8781dac1bdaa886e4003e4985
bbhjx <bnl121470@gmail.com>
2019-05-01 15:41:26 -0500

    new app

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'wtfever',
+            'author':'Brittni LaGrand',
+            'create_date':'04-25-2019'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'loremLorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aliquam facilis sequi rem optio molestias, mollitia necessitatibus architecto consequatur vero odio quidem velit exercitationem? Fugit eveniet alias ducimus iste laborum?',
+            'author':'Raj LaGrand',
+            'create_date':'04-25-2019'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'loremLorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aliquam facilis sequi rem optio molestias, mollitia necessitatibus architecto consequatur vero odio quidem velit exercitationem? Fugit eveniet alias ducimus iste laborum?',
+            'author':'Brittni LaGrand',
+            'create_date':'04-25-2019'
+        }
+    ]
+    return articles
